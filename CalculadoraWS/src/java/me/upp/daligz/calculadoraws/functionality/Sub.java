f37f255ead34a0024974052e30a425a014b19972
Daligz <oscardali370@gmail.com>
2022-05-08 13:57:55 -0500

    Sub operation implemented

---
+++ CalculadoraWS/src/java/me/upp/daligz/calculadoraws/functionality/Sub.java
@@ -0,0 +1,9 @@
+package me.upp.daligz.calculadoraws.functionality;
+
+public class Sub extends Calculator {
+
+    @Override
+    public double operation(int num1, int num2) {
+        return (num1 - num2);
+    }
+}
