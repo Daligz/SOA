2951b059e9f722212297ef1042021c9e560f015f
Daligz <oscardali370@gmail.com>
2022-05-08 14:08:12 -0500

    Web service calculator implemented

---
+++ CalculadoraWS/src/java/me/upp/daligz/calculadoraws/service/WSCalculator.java
@@ -0,0 +1,34 @@
+package me.upp.daligz.calculadoraws.service;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import me.upp.daligz.calculadoraws.functionality.Div;
+import me.upp.daligz.calculadoraws.functionality.Mul;
+import me.upp.daligz.calculadoraws.functionality.Sub;
+import me.upp.daligz.calculadoraws.functionality.Sum;
+
+@WebService(serviceName = "WSCalculator")
+public class WSCalculator {
+
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    public double suma(@WebParam(name = "num1") int num1, @WebParam(name = "num2") int num2) {
+        return new Sum().operation(num1, num2);
+    }
+    
+    public double resta(@WebParam(name = "num1") int num1, @WebParam(name = "num2") int num2) {
+        return new Sub().operation(num1, num2);
+    }
+    
+    public double division(@WebParam(name = "num1") int num1, @WebParam(name = "num2") int num2) {
+        return new Div().operation(num1, num2);
+    }
+    
+    public double multiplicacion(@WebParam(name = "num1") int num1, @WebParam(name = "num2") int num2) {
+        return new Mul().operation(num1, num2);
+    }
+}
