504d5044cc16cf0f1f70bd5f67d17e3e43188551
Daligz <oscardali370@gmail.com>
2022-05-11 11:27:20 -0500

    Project calculator example created and funciontality pre-implemented

---
+++ CalculadoraWS_2/src/java/me/upp/daligz/calculadoraws2/pkgFuncionalidad/Calculadora.java
@@ -0,0 +1,5 @@
+package me.upp.daligz.calculadoraws2.pkgFuncionalidad;
+
+public abstract class Calculadora {
+    public abstract double calcular(final int num1, final int num2);
+}
---
+++ CalculadoraWS_2/src/java/me/upp/daligz/calculadoraws2/pkgFuncionalidad/Sum.java
@@ -0,0 +1,9 @@
+package me.upp.daligz.calculadoraws2.pkgFuncionalidad;
+
+public class Sum extends Calculadora {
+
+    @Override
+    public double calcular(int num1, int num2) {
+        return (num1 + num2);
+    }
+}
