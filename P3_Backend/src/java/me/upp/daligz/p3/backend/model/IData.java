2bc44cd726375a7b61eee16151bb6f7d2639f413
Daligz <oscardali370@gmail.com>
2022-05-21 14:53:29 -0500

    Read method implemented and update value to CompletableFuture

--- P3_Backend/src/java/me/upp/daligz/p3/backend/model/IData.java
+++ P3_Backend/src/java/me/upp/daligz/p3/backend/model/IData.java
@@ -1,8 +1,10 @@
 package me.upp.daligz.p3.backend.model;
 
+import java.util.concurrent.CompletableFuture;
+
 public interface IData<T> {
     void create(final String name, final String user, final String password);
-    T read(final String id);
+    CompletableFuture<T> read(final String id);
     void update(final String value, final String valueToUpdate);
     void delete(final String id);
 }
--- P3_Backend/src/java/me/upp/daligz/p3/backend/model/Model.java
+++ P3_Backend/src/java/me/upp/daligz/p3/backend/model/Model.java
@@ -1,10 +1,13 @@
 package me.upp.daligz.p3.backend.model;
 
+import java.sql.ResultSet;
+import java.util.concurrent.CompletableFuture;
 import me.upp.daligz.p3.backend.model.commons.User;
 import me.upp.daligz.p3.backend.model.tables.TblUsers;
 import me.upp.daligz.p3.backend.modelo.SimpleSourceBuilder;
 import net.royalmind.library.lightquery.HikariPool;
 import net.royalmind.library.lightquery.queries.LInsert;
+import net.royalmind.library.lightquery.queries.LSelect;
 
 public class Model implements IData<User> {
 
@@ -26,8 +29,28 @@ public class Model implements IData<User> {
     }
 
     @Override
-    public User read(final String id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public CompletableFuture<User> read(final String id) {
+        final String query = new LSelect()
+                .from(TblUsers.TBL_NAME.getValue())
+                .value("*")
+                .where(TblUsers.ID.getValue(), "=", id)
+                .getQuery();
+        final CompletableFuture<User> future = new CompletableFuture<>();
+        HIKARI_POOL.execute(conn -> {
+            final ResultSet result = conn.prepareStatement(query).executeQuery();
+            if (result.first()) {
+                future.complete(new User(
+                        result.getString(TblUsers.NAME.getValue()),
+                        result.getString(TblUsers.USER.getValue()),
+                        result.getInt(TblUsers.ID.getValue()),
+                        result.getInt(TblUsers.PASSWORD.getValue())
+                ));
+            } else {
+                future.complete(null);
+            }
+            return null;
+        });
+        return future;
     }
 
     @Override
