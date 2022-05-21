9cece8ba33ea5e4ed2ba630687414b9e17ad7129
Daligz <oscardali370@gmail.com>
2022-05-21 12:43:24 -0500

    Model fix

--- P3_Backend/src/java/me/upp/daligz/p3/backend/modelo/SourceBuilder.java
+++ P3_Backend/src/java/me/upp/daligz/p3/backend/modelo/SourceBuilder.java
@@ -6,8 +6,6 @@ public class SourceBuilder {
     
     private String url = "", user = "", password = "";
     
-    private final HikariConfig hikariConfig = new HikariConfig();
-    
     public SourceBuilder setUlr(final String url) {
         this.url = url;
         return this;
@@ -24,13 +22,14 @@ public class SourceBuilder {
     }
     
     public HikariConfig build() {
-        if (this.url.isEmpty() || this.user.isEmpty() || this.password.isEmpty()) return null;
-        this.hikariConfig.setJdbcUrl(this.url);
-        this.hikariConfig.setUsername(this.user);
-        this.hikariConfig.setPassword(this.password);
-        this.hikariConfig.addDataSourceProperty("cachePrepStmts", true);
-        this.hikariConfig.addDataSourceProperty("prepStmtCacheSize", 250);
-        this.hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
-        return this.hikariConfig;
+        if (this.url.isEmpty() || this.user.isEmpty()) return null;
+        final HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(this.url);
+        hikariConfig.setUsername(this.user);
+        hikariConfig.setPassword(this.password);
+        hikariConfig.addDataSourceProperty("cachePrepStmts", true);
+        hikariConfig.addDataSourceProperty("prepStmtCacheSize", 250);
+        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
+        return hikariConfig;
     }
 }
