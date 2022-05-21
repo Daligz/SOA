d4ecf384637d74d4b8ceaedef2baf16ee5a201ad
Daligz <oscardali370@gmail.com>
2022-05-21 18:58:55 -0500

    Controller implemented

--- P3_Client/src/java/me/upp/daligz/p3/client/controller/Controller.java
+++ P3_Client/src/java/me/upp/daligz/p3/client/controller/Controller.java
@@ -1,5 +1,36 @@
 package me.upp.daligz.p3.client.controller;
 
-public class Controller {
+import com.google.gson.Gson;
+import me.upp.daligz.p3.backend.model.WSDatabase;
+import me.upp.daligz.p3.backend.model.WSDatabase_Service;
+import me.upp.daligz.p3.backend.model.commons.User;
+
+public class Controller implements IData<User>{
     
+    private final WSDatabase wsd;
+    private static final Gson GSON = new Gson();
+
+    public Controller() {
+        this.wsd = new WSDatabase_Service().getWSDatabasePort();
+    }
+    
+    @Override
+    public void create(final String name, final String user, final int password) {
+        this.wsd.create(name, user, password);
+    }
+
+    @Override
+    public User read(final int id) {
+        return GSON.fromJson(this.wsd.read(id), User.class);
+    }
+    
+    @Override
+    public void update(final int id, final String value, final String valueToUpdate) {
+        this.wsd.update(id, value, valueToUpdate);
+    }
+
+    @Override
+    public void delete(final int id) {
+        this.wsd.delete(id);
+    }
 }
---
+++ P3_Client/src/java/me/upp/daligz/p3/client/controller/IData.java
@@ -0,0 +1,8 @@
+package me.upp.daligz.p3.client.controller;
+
+public interface IData<T> {
+    void create(final String name, final String user, final int password);
+    T read(final int id);
+    void update(final int id, final String value, final String valueToUpdate);
+    void delete(final int id);
+}
