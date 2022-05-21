8eb24876282ab58f996e8a5605ba9010f8cc23c3
Daligz <oscardali370@gmail.com>
2022-05-21 11:45:25 -0500

    Source builder and model pre-implemented

--- P3_Backend/src/java/me/upp/daligz/p3/backend/modelo/Model.java
+++ P3_Backend/src/java/me/upp/daligz/p3/backend/modelo/Model.java
@@ -1,5 +1,16 @@
 package me.upp.daligz.p3.backend.modelo;
 
+import net.royalmind.library.lightquery.HikariPool;
+
 public class Model {
 
+    private static final HikariPool HIKARI_POOL = new HikariPool(
+            new SourceBuilder()
+                    .setUlr("jdbc:mysql://localhost:6868/bddatos?useSSL=false")
+                    .setUser("root")
+                    .setPassword("")
+                    .build()
+    );
+    
+    
 }
--- P3_Backend/src/java/me/upp/daligz/p3/backend/modelo/SourceBuilder.java
+++ P3_Backend/src/java/me/upp/daligz/p3/backend/modelo/SourceBuilder.java
@@ -1,7 +1,36 @@
 package me.upp.daligz.p3.backend.modelo;
 
+import com.zaxxer.hikari.HikariConfig;
+
 public class SourceBuilder {
     
+    private String url = "", user = "", password = "";
     
+    private final HikariConfig hikariConfig = new HikariConfig();
     
+    public SourceBuilder setUlr(final String url) {
+        this.url = url;
+        return this;
+    }
+    
+    public SourceBuilder setUser(final String user) {
+        this.user = user;
+        return this;
+    }
+    
+    public SourceBuilder setPassword(final String password) {
+        this.password = password;
+        return this;
+    }
+    
+    public HikariConfig build() {
+        if (this.url.isEmpty() || this.user.isEmpty() || this.password.isEmpty()) return null;
+        this.hikariConfig.setJdbcUrl(this.url);
+        this.hikariConfig.setUsername(this.user);
+        this.hikariConfig.setPassword(this.password);
+        this.hikariConfig.addDataSourceProperty("cachePrepStmts", true);
+        this.hikariConfig.addDataSourceProperty("prepStmtCacheSize", 250);
+        this.hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
+        return this.hikariConfig;
+    }
 }
