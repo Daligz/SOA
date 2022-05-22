630d0458b053b16d29e47cb96fa3d3ec162212d2
Daligz <oscardali370@gmail.com>
2022-05-21 20:11:05 -0500

    Create button implemented

--- P3_Client/src/java/me/upp/daligz/p3/client/controller/Controller.java
+++ P3_Client/src/java/me/upp/daligz/p3/client/controller/Controller.java
@@ -1,21 +1,53 @@
 package me.upp.daligz.p3.client.controller;
 
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 import me.upp.daligz.p3.client.model.Model;
+import me.upp.daligz.p3.client.model.commons.User;
 import me.upp.daligz.p3.client.view.View;
 
 public class Controller {
     
     private final Model model;
-    private View view;
+    private final View view;
 
     public Controller(final Model model, final View view) {
         this.model = model;
         this.view = view;
+        this.registerActions();
     }
     
-    public void create() {
-        view.getBtnRead().addActionListener((e) -> {
-            
+    private void registerActions() {
+        this.create();
+    }
+    
+    private void create() {
+        this.view.getBtnCreate().addActionListener((e) -> {
+            if (isEmpty(this.view.getTxtName(), this.view.getTxtPassword(), this.view.getTxtUser())) {
+                this.alert("Error", "No puedes dejar campos vacios!", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            this.model.create(
+                    this.view.getTxtName().getText(), 
+                    this.view.getTxtPassword().getText(),
+                    Integer.valueOf(this.view.getTxtPassword().getText())
+            );
+            this.alert("Exitoso", "Se agrego un nuevo usuario!", JOptionPane.INFORMATION_MESSAGE);
         });
+    }
+    
+    private void alert(final String title, final String message, final int jOptionPaneIcon) {
+        JOptionPane.showMessageDialog(null, message, title, jOptionPaneIcon);
+    }
+    
+    private boolean isEmpty(final JTextField... fields) {
+        for (final JTextField field : fields) if (field.getText().isEmpty()) return true;
+        return false;
+    }
+    
+    private void clear() {
+        this.view.getTxtName().setText("");
+        this.view.getTxtPassword().setText("");
+        this.view.getTxtUser().setText("");
     }
 }
