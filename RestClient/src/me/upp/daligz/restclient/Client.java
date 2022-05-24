d8b0fd0b25137183f0831f30cbb58c188cae133e
Daligz <oscardali370@gmail.com>
2022-05-23 21:56:09 -0500

    Pom fix with dependencies

--- RestClient/src/me/upp/daligz/restclient/Client.java
+++ RestClient/src/me/upp/daligz/restclient/Client.java
@@ -1,5 +1,19 @@
 package me.upp.daligz.restclient;
 
+import me.upp.library.intelligod.IntelliGod;
+
 public class Client {
+    
+    private static final IntelliGod ROOT = IntelliGod.init(
+        new SimpleSourceBuilder()
+                .setUlr("jdbc:mysql://localhost:3306/bddatos?useSSL=false")
+                .setUser("root")
+                .setPassword("")
+                .build()
+    );
+    
+    public static void main(final String[] args) {
+        IntelliGod.init(args)
+    }
     
 }
