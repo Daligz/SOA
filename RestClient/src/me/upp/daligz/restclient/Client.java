6a059f8e2bdbac4031a8cd6a64f1dbe7b82a1c92
Daligz <oscardali370@gmail.com>
2022-05-26 10:13:53 -0500

    MVC structure added to client and View implemented

--- RestClient/src/me/upp/daligz/restclient/Client.java
+++ RestClient/src/me/upp/daligz/restclient/Client.java
@@ -1,5 +1,26 @@
 package me.upp.daligz.restclient;
 
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import me.upp.daligz.restclient.view.View;
+
 public class Client {
-    
+
+    public static void main(final String args[]) {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (final ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(View.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(() -> {
+            new View().setVisible(true);
+        });
+    }
 }
