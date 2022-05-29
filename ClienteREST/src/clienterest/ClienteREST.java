d29ca2ae2cf3dcb1d18104b3cb857c2627ea0a82
Daligz <oscardali370@gmail.com>
2022-05-29 14:54:19 -0500

    Booter moved to main class

--- ClienteREST/src/clienterest/ClienteREST.java
+++ ClienteREST/src/clienterest/ClienteREST.java
@@ -1,8 +1,27 @@
 package clienterest;
 
+import java.awt.EventQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import view.View;
+
 public class ClienteREST {
 
     public static void main(String[] args) {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(View.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
+        EventQueue.invokeLater(() -> {
+            new View().setVisible(true);
+        });
     }    
 }
