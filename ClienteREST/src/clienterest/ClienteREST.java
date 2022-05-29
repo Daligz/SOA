29feb03e44bb2aa37b06afd77db1c248194deb63
Daligz <oscardali370@gmail.com>
2022-05-29 16:25:21 -0500

    MVC implemented to booter

--- ClienteREST/src/clienterest/ClienteREST.java
+++ ClienteREST/src/clienterest/ClienteREST.java
@@ -1,9 +1,12 @@
 package clienterest;
 
+import controller.Controller;
 import java.awt.EventQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.UIManager;
+import model.Model;
+import pkgRest.NewJerseyClient;
 import view.View;
 
 public class ClienteREST {
@@ -16,12 +19,15 @@ public class ClienteREST {
                     break;
                 }
             }
-        } catch (Exception ex) {
+        } catch (final Exception ex) {
             Logger.getLogger(View.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         EventQueue.invokeLater(() -> {
-            new View().setVisible(true);
+            final Model model = new Model(new NewJerseyClient());
+            final View view = new View();
+            final Controller controller = new Controller(model, view);
+            view.setVisible(true);
         });
     }    
 }
