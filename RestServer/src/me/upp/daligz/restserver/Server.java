b23792a5ea1ce0620b273dea4549889e721599d7
Daligz <oscardali370@gmail.com>
2022-06-05 19:18:49 -0500

    Implementation fix

--- RestServer/src/me/upp/daligz/restserver/Server.java
+++ RestServer/src/me/upp/daligz/restserver/Server.java
@@ -1,14 +1,14 @@
 package me.upp.daligz.restserver;
 
-import me.upp.daligz.p3.backend.modelo.SimpleSourceBuilder;
 import me.upp.library.intelligod.IntelliGod;
+import net.royalmind.library.lightquery.SimpleSourceBuilder;
 import spark.Spark;
 
 public class Server {
     
     private static final IntelliGod ROOT = IntelliGod.init(
         new SimpleSourceBuilder()
-            .setUlr("jdbc:mysql://localhost:3306/bddatos?useSSL=false")
+            .setUrl("jdbc:mysql://localhost:3306/bddatos?useSSL=false")
             .setUser("root")
             .setPassword("")
             .build()
